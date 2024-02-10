2c09f86e682da1b6900200ee9de0d0a8e8c7def9
kokilavinoth243 <kokilavinoth243@gmail.com>
2024-02-10 10:22:44 +0530

    code by story3

--- src/org/demoTest/Test1.java
+++ src/org/demoTest/Test1.java
@@ -1,9 +1,15 @@
 package org.demoTest;
 
 public class Test1 {
+	private void login() {
+	System.out.println("Login GitHub");
+
+	}
 
 	public static void main(String[] args) {
 		System.out.println("This is simple");
 		System.out.println("This is specially for GitHub");
+		Test1 t1=new Test1();
+		t1.login();
 	}
 }
